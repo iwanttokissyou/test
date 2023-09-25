a72870f0d2b2812ea741c59fb7703a3f3eb139f6
iwanttokissyou <yasithamahesh123@gmail.com>
2023-09-24 17:47:07 -0700

    constructors

--- main.cpp
+++ main.cpp
@@ -17,24 +17,40 @@ class Employee {
     private are only accessible inside the class, 
     public are only accessible anywhere
     */
-public:
-    string name;
-    string company;
-    int age;
+   /*
+   ****THREE RULES OF CONSTRUCTORS
+    1. A METHOD, BUT DOES NOT HAVE A RETURN 
+    2. IT WILL HAVE THE SAME NAME AS CLASS
+    3. SHOULD BE PUBLIC, THERE ARE SOME CASES CONSTRUCTORS CAN BE PRIVATE
+   */ 
+
     /* function inside a class is a class method*/
+public:
+    string Name;  
+    string Company;
+    int Age;
+
     void introduce(){
-        std::cout << "Name" << name << std::endl;
-        std::cout << "company" << company << std::endl;
-        std::cout << "age" << age << std::endl;
+        std::cout << "Name " << Name << std::endl;
+        std::cout << "company " << Company << std::endl;
+        std::cout << "age " << Age << std::endl;
+    }
+    Employee(string name, string company, int age) { /*the sttribute name and function argurements have to be odfferent I dont know why either yet!*/
+        Name = name;
+        Company = company;
+        Age = age;
     }
 };
 
 int main()
 {
-    Employee empl1;// an boject of class Employee
+   /* Employee empl1;// an boject of class Employee aka default constructor if there is no contructor has been made for this class
     empl1.name = "Yasitha";
     empl1.age = 55;
     empl1.company = "hellwork";
-
+*/
+    Employee empl1 = Employee("yasitha","hellwork",33);
+    Employee empl2 = Employee("mahesh","shitwork",33);
     empl1.introduce();
+    empl2.introduce();
 }
