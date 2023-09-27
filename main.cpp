0e38b1bc235a4ee3c70e723455a5e3c8a46826e9
iwanttokissyou <yasithamahesh123@gmail.com>
2023-09-26 21:45:11 -0700

    basics of  inheritance

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@ attributes are things like age and name etc,
 behaviours are like things it can do ex: tell the name age and the company
 */
 
-class Employee {
+class AbstractEmployee { // this is an abstract class because it has atleast one pure virtual function
+    public:
+        virtual void AskforPromotion() = 0;
+};
+
+class Employee : public AbstractEmployee{
     /*
         by default all the attributes will be private otheriwse speicifed
         so the attributes should be public if we dont use cnotructors or  gets and sets functions
@@ -25,7 +30,7 @@ class Employee {
    */ 
 
     /* function inside a class is a class method*/
-private: // in this way we have hidden these three variables aka encapsulated, if we want to access or modifiy the data we will have to impliment the functions for these data aka we will have to implement setters and getters
+public: // in this way we have hidden these three variables aka encapsulated, if we want to access or modifiy the data we will have to impliment the functions for these data aka we will have to implement setters and getters
     string Name;  
     string Company;
     int Age;
@@ -41,28 +46,30 @@ public:
         std::cout << "company " << Company << std::endl;
         std::cout << "age " << Age << std::endl;
     }
-    Employee(string name, string company, int age) { /*the sttribute name and function argurements have to be odfferent I dont know why either yet!*/
-        Name = name;
+    void AskforPromotion(){
+        if (Age > 25 ) {
+            std::cout << "promoted" << std::endl;
+        }
+    }
+   /* Employee(string name, string company, int age) { /*the sttribute name and function argurements have to be odfferent I dont know why either yet!*/
+   /*     Name = name;
         Company = company;
         Age = age;
+    }*/
+};
+
+/*
+this developer class is inheritaned from Employee class. therefore it will have all the attributes and behviours from the main Employee class.
+*/
+class developer : public Employee { // if we dont make this public, the object we are going to creat in the main function will not have the access to attributes and behaviours
+    public :
+     string Language;
+     developer(string name, string company, int age, string lang){// in this case we haven't implmeneted a constructor in the Employee class so we can directly work like this
+        Name = name;
+        Company = company;
+        Age = age; 
+        Language  = lang;
     }
-};
-
-
-/*concept of virtual functions*/
-class base {
-    public:
-        virtual void print()=0; // this is a pure virtual function and then this base class become an abstrct class, a class is an abstract class if there is atleast one pure virtual function
-};
-
-class derive : public base {
-    public: 
-        void print() {
-            std::cout << "print this derive\n";    
-        }
-        void show() {
-            std:: cout << "show this derive\n";
-        } 
 };
 
 int main()
@@ -72,18 +79,17 @@ int main()
     empl1.age = 55;
     empl1.company = "hellwork";
 */
-    Employee empl1 = Employee("yasitha","hellwork",33);
+ /*   Employee empl1 = Employee("yasitha","hellwork",33);
     Employee empl2 = Employee("mahesh","shitwork",33);
     empl1.introduce();
-    empl2.introduce();
+    empl2.introduce();*/
 
-    std::cout << "name of the first emplyee is  " << empl1.getName() << std::endl;
+    
 
-    base* base_class_pointer; // we are using pointers here because we need to change somethnig in runtime,
-    derive derive_class;
-    base_class_pointer = &derive_class;
+    developer developer_1 = developer("yasitha","hell",22,"python");
 
-    base_class_pointer -> print(); // this  will replace the print function in the base class in runtime
-    //base_class_pointer -> show(); // this will replace the show function in compile time.
+std::cout << "name of the first emplyee is  " << developer_1.Name << std::endl;
+
+developer_1.introduce();
 
 }
